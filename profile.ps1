8ec92c65c17fbdd0c62fa30254fd95ddff5f7686
Joeky Zhan <jj16180339887@gmail.com>
2018-03-22 19:51:23 +0800

    Use systeminfo
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

--- profile.ps1
+++ profile.ps1
@@ -168,23 +168,7 @@ Function touch {
 }
 Function uname {
   try {
-    $computerSystem = Get-CimInstance CIM_ComputerSystem
-    $computerBIOS   = Get-CimInstance CIM_BIOSElement
-    $computerOS     = Get-CimInstance CIM_OperatingSystem
-    $computerCPU    = Get-CimInstance CIM_Processor
-    $computerHDD    = Get-CimInstance Win32_LogicalDisk -Filter "DeviceID = 'C:'"
-
-    Write-Host "System Information for: " $computerSystem.Name -BackgroundColor DarkRed
-    "Manufacturer: " + $computerSystem.Manufacturer
-    "Model: " + $computerSystem.Model
-    "Serial Number: " + $computerBIOS.SerialNumber
-    "CPU: " + $computerCPU.Name
-    "HDD Capacity: "  + "{0:N2}" -f ($computerHDD.Size/1GB) + "GB"
-    "HDD Space: " + "{0:P2}" -f ($computerHDD.FreeSpace/$computerHDD.Size) + " Free (" + "{0:N2}" -f ($computerHDD.FreeSpace/1GB) + "GB)"
-    "RAM: " + "{0:N2}" -f ($computerSystem.TotalPhysicalMemory/1GB) + "GB"
-    "Operating System: " + $computerOS.caption + ", Service Pack: " + $computerOS.ServicePackMajorVersion
-    "User logged In: " + $computerSystem.UserName
-    "Last Reboot: " + $computerOS.LastBootUpTime
+    systeminfo
     Write-Host "All of the Graphics cards Informations " -BackgroundColor DarkRed
     wmic path win32_VideoController get Name
     wmic path win32_VideoController get VideoModeDescription
