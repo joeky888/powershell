549bdd177c9f6a6f012f93ccaf6ebac20c51d8ae
Joeky Zhan <jj16180339887@gmail.com>
2019-05-04 11:23:01 +0800

    Increase mpv cache time
    Signed-off-by: Joeky <jj16180339887@gmail.com>

--- profile.ps1
+++ profile.ps1
@@ -494,10 +494,10 @@ Function youtube-dl-mp3 {
 set-alias mp3 youtube-dl-mp3
 
 Function mpv-1080 {
-  mpv.exe --ytdl-format="bestvideo[height<=1080][fps<=30]+bestaudio/best" --cache=1048576 --cache-secs=1200 $args
+  mpv.exe --ytdl-format="bestvideo[height<=1080][fps<=30]+bestaudio/best" --cache=1048576 --cache-secs=1800 $args
 }
 Function mpv-720 {
-  mpv.exe --ytdl-format="bestvideo[height<=720][fps<=30]+bestaudio/best" --cache=600000 --cache-secs=1200 $args
+  mpv.exe --ytdl-format="bestvideo[height<=720][fps<=30]+bestaudio/best" --cache=600000 --cache-secs=1800 $args
 }
 Function streamlink-mpv-1080 {
   streamlink.exe --verbose-player --player 'mpv --cache=600000' --default-stream 1080p $args
