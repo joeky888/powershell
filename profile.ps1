ed9375826c74b8b44fc0af8064e87ff2c14f90a0
Joeky Zhan <jj16180339887@gmail.com>
2019-08-11 20:39:43 +0800

    Update path
    Signed-off-by: Joeky <jj16180339887@gmail.com>

--- profile.ps1
+++ profile.ps1
@@ -611,9 +611,9 @@ if ($c3 -ne "") {
   }
 }
 
-$NPM_PACKAGES="$env:USERPROFILE/.npm-packages"
-$env:PATH=$NPM_PACKAGES/bin;$PATH
-$env:NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+$NPM_PACKAGES="$env:USERPROFILE\.npm-packages"
+$env:PATH="$NPM_PACKAGES\bin;$env:PATH"
+$env:NODE_PATH="$NPM_PACKAGES\lib\node_modules:$NODE_PATH"
 Function upgradeNpm {
   npm install -g npm@latest
   npm update -g
