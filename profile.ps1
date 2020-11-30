2c4b273dbc946f4ca9fad3c256a9ad4ee6182f23
Joeky <joeky5888@gmail.com>
2020-11-30 11:12:25 +0800

    Use new command instead
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- profile.ps1
+++ profile.ps1
@@ -506,10 +506,13 @@ Function youtube-dl-mp3 {
 set-alias mp3 youtube-dl-mp3
 
 Function mpv-1080 {
-  mpv.com --script-opts="ytdl_hook-ytdl_path=youtube-dl" --ytdl-format="bestvideo[height<=1080][fps<=30]+bestaudio/best" --cache=yes --cache-dir=$env:TEMP --cache-on-disk=yes --ytdl-raw-options="no-check-certificate=,yes-playlist=,hls-prefer-native=,ignore-errors=,write-auto-sub=,write-sub=" "$args"
+  mpv.com --script-opts="ytdl_hook-ytdl_path=youtube-dl" --ytdl-format="bestvideo[height<=1080][fps<=30]+bestaudio/best" --cache=yes --cache-dir=$env:TEMP --cache-on-disk=yes --ytdl-raw-options="no-check-certificate=,yes-playlist=,hls-prefer-native=,ignore-errors=" "$args"
 }
 Function mpv-720 {
-  mpv.com --script-opts="ytdl_hook-ytdl_path=youtube-dl" --ytdl-format="bestvideo[height<=720][fps<=30]+bestaudio/best" --cache=yes --cache-dir=$env:TEMP --cache-on-disk=yes --ytdl-raw-options="no-check-certificate=,yes-playlist=,hls-prefer-native=,ignore-errors=,write-auto-sub=,write-sub=" "$args"
+  mpv.com --script-opts="ytdl_hook-ytdl_path=youtube-dl" --ytdl-format="bestvideo[height<=720][fps<=30]+bestaudio/best" --cache=yes --cache-dir=$env:TEMP --cache-on-disk=yes --ytdl-raw-options="no-check-certificate=,yes-playlist=,hls-prefer-native=,ignore-errors=" "$args"
+}
+Function mpv-1080-auto-sub {
+  mpv.com --script-opts="ytdl_hook-ytdl_path=youtube-dl" --ytdl-format="bestvideo[height<=1080][fps<=30]+bestaudio/best" --cache=yes --cache-dir=$env:TEMP --cache-on-disk=yes --ytdl-raw-options="no-check-certificate=,yes-playlist=,hls-prefer-native=,ignore-errors=,write-auto-sub=,write-sub=,sub-lang=en" "$args"
 }
 Function streamlink-mpv-1080 {
   streamlink.exe --verbose-player --player 'mpv.com --cache=yes' --default-stream 1080p "$args"
