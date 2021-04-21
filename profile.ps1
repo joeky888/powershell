7b62b62020fb19407708ac38b27c47e39a3000a6
Joeky <joeky5888@gmail.com>
2021-04-21 23:17:39 +0800

    Streamlink add {title}
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- profile.ps1
+++ profile.ps1
@@ -527,10 +527,10 @@ Function mpv-1080-auto-sub {
   mpv.com --osd-font="Microsoft YaHei" --script-opts="ytdl_hook-ytdl_path=youtube-dl" --ytdl-format="bestvideo[height<=1080][fps<=30]+bestaudio/best" --cache=yes --cache-dir=$env:TEMP --cache-on-disk=yes --ytdl-raw-options="no-check-certificate=,yes-playlist=,hls-prefer-native=,ignore-errors=,write-auto-sub=,write-sub=,sub-lang=en" "$args"
 }
 Function streamlink-mpv-1080 {
-  streamlink.exe --verbose-player --player 'mpv.com --osd-font="Microsoft YaHei" --cache=yes' --default-stream 1080p "$args"
+  streamlink.exe --verbose-player --player 'mpv.com --osd-font="Microsoft YaHei" --cache=yes' --title '{title}' --default-stream 1080p "$args"
 }
 Function streamlink-mpv-720 {
-  streamlink.exe --verbose-player --player 'mpv.com --osd-font="Microsoft YaHei" --cache=yes' --default-stream 720p "$args"
+  streamlink.exe --verbose-player --player 'mpv.com --osd-font="Microsoft YaHei" --cache=yes' --title '{title}' --default-stream 720p "$args"
 }
 
 Function Reset-Networking {
